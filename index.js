b1d3dace77baf76623690cd4347f28e9c3f6c8ea
Gabriela <gabriela.nogami@hotmail.com>
2026-04-30 08:38:17 -0300

    Bug fix

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-    import express from 'express';
-    import cors from 'cors';
+import express from 'express';
+import cors from 'cors';
 
-    const app = express();
+const app = express();
+app.use(cors());
 
-    app.use(cors());
-    //post delete put 
-    app.get('/', (req, res) => {//Request pode ser acompanhada com um ou mais parametros ex num1 e num2 - escolher operação estou passando 2 numeros dai a funçao que está dentro dessa parte
-        res.json({
-            date: new Date().toLocaleString('pt-BR'),
-            status: 'API no Render funcionando!'
-        });
-    });
+app.get('/datetime', (req, res) => {
+  const now = new Date();
 
-    // Porta dinâmica para o Render
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
-    });
+  res.json({
+    date: now.toLocaleDateString('pt-BR'),
+    time: now.toLocaleTimeString('pt-BR')
+  });
+});
+
+// calculadora simples
+app.get('/calculate', (req, res) => {
+  const { a, b } = req.query;
+
+  const numA = Number(a);
+  const numB = Number(b);
+
+  res.json({
+    result: numA + numB
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
